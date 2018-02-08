594df81b6de45344b444bcefda9f9402be5e37d1
sigrundish <sigrun@dohop.com>
2018-02-08 18:33:21 +0000

    initial commit

---
+++ app/src/main/java/com/example/sigrundish/actio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sigrundish.actio;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Your key is a public constant EXTRA_MESSAGE because the next activity uses the key to retrieve the text value.
+    //It's a good practice to define keys for intent extras using your app's package name as a prefix.
+    public static final String EXTRA_MESSAGE = "com.example.sigrundish.actio.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
