43d946ee458224df197e667019af3acbbc43b694
Markús Freyr <markusfreyr@tg-dw076.rhi.hi.is>
2018-02-13 15:32:56 +0000

    bla

--- app/src/main/java/com/example/sigrundish/actio/MainActivity.java
+++ app/src/main/java/com/example/sigrundish/actio/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.sigrundish.actio;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
     //Your key is a public constant EXTRA_MESSAGE because the next activity uses the key to retrieve the text value.
@@ -15,6 +19,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(loginIntent);
+            }
+        });
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Intent registerIntent = new Intent(MainActivity.this, Register.class);
+                //MainActivity.this.startActivity(registerIntent);
+                final Client socket1 = new Client("127.0.0.1", 1234);
+                socket1.setClientCallback(new Client.ClientCallback () {
+                    @Override
+                    public void onMessage(String message) {
+                        System.out.println("1");
+                    }
+
+                    @Override
+                    public void onConnect(Socket socket) {
+                        System.out.println("2");
+                        socket1.send("Hello World!\n");
+                        socket1.disconnect();
+                    }
+
+                    @Override
+                    public void onDisconnect(Socket socket, String message) {
+                        System.out.println("3");
+
+                    }
+
+                    @Override
+                    public void onConnectError(Socket socket, String message) {
+                        System.out.println("4");
+                    }
+                });
+
+                socket1.connect();
+
+
+            }
+        });
+
+
+
     }
 
     /** Called when the user taps the Send button */
@@ -24,5 +80,10 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/sigrundish/actio/Register.java
+++ app/src/main/java/com/example/sigrundish/actio/Register.java
@@ -1,7 +1,9 @@
 package com.example.sigrundish.actio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+
     }
 }
